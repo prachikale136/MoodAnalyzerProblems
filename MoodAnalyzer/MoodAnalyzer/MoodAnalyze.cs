fce143e1a3837def2279eb897eff6f6909e4459c
Prachi <prachikale136@gmail.com>
2023-06-29 22:03:03 +0530

    [Prachi] Add: inform user if enterd invalid mood

--- MoodAnalyzer/MoodAnalyzer/MoodAnalyze.cs
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyze.cs
@@ -15,16 +15,33 @@ namespace MoodAnalyzer
             //this indicate difff of global name and method parrameter
             this.Message = message;
         }
+
+        
+
         //method for MoodAnalyzer
         public string AnalyzeMood()
         {
-            //if message contains sad
-            if (this.Message.Contains("Sad"))
-                // it will retrun sad
-                return "Sad";
-            else
-                //else happy
-                return "Happy";
+            //UC2
+            try
+            {
+                if (this.Message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionsType.EMPTY_MESSAGE, "Message should not be Empty");
+                }
+                // put the code here that may raise exceptions
+                //if message contains sad
+                if (this.Message.Contains("Sad"))
+                    // it will retrun sad
+                    return "Sad";
+                else
+                    //else happy
+                    return "Happy";
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionsType.NULL_MESSAGE, "Message should not be Null");
+            }
+            
         }
 
     }
--- MoodAnalyzer/MoodAnalyzerTest/UnitTest1.cs
+++ MoodAnalyzer/MoodAnalyzerTest/UnitTest1.cs
@@ -24,5 +24,54 @@ namespace MoodAnalyzerTest
             //assertion is a boolean expression at a specific point in a program 
             Assert.AreEqual(expected, moodAnalyzer.AnalyzeMood());
         }
+        public void GivenNullMoodShouldReturnHappy()
+        {
+            try
+            {
+                //expected string will be happy
+                string expected = "Happy";
+                //create a object of cass moodanalyzer with Null value
+                MoodAnalyze moodAnalyzer = new MoodAnalyze(null);
+                //assertion is a boolean expression at a specific point in a program 
+                Assert.AreEqual(expected, moodAnalyzer.AnalyzeMood());
+            }
+            catch (MoodAnalyzerCustomException e)
+            {
+                Assert.AreEqual(MoodAnalyzerCustomException.ExceptionsType.NULL_MESSAGE, e.type);
+            }
+        }
+        [Test]
+        // [DataRow(" ")]
+        public void GivenEmptyMood_ShouldThrow_MoodAnalysisEmptyTypeException()
+        {
+            try
+            {
+                string expected = "Happy";
+                string message = "";
+                MoodAnalyze moodAnalyzer = new MoodAnalyze(message);
+                Assert.AreEqual(expected, moodAnalyzer.AnalyzeMood());
+            }
+            catch (MoodAnalyzerCustomException e)
+            {
+                Assert.AreEqual("Message should not be Empty", e.Message);
+            }
+        }
+        [Test]
+        // [DataRow(null)]
+        public void GivenNullMood_ShouldThrow_MoodAnalysisEmptyTypeException()
+        {
+            try
+            {
+                string expected = "Happy";
+                string message = null;
+                MoodAnalyze moodAnalyzer = new MoodAnalyze(message);
+                Assert.AreEqual(expected, moodAnalyzer.AnalyzeMood());
+            }
+            catch (MoodAnalyzerCustomException e)
+            {
+                Assert.AreEqual("Message should not be Null", e.Message);
+            }
+        }
+
     }
 }
